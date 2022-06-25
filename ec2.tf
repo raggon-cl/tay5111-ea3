3e4e2b52e1ff89574dde7b93305729538e0c2363
Rodrigo Aguilar <r.aguilarg@profesor.duoc.cl>
2022-06-25 21:03:03 +0000

    Add subnet and SG in ec2.tf file

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,10 @@ resource "aws_instance" "server1a" {
   ami           = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
   instance_type = "t2.micro"
   key_name      = "vockey"
-  
+  subnet_id     = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+
   tags = {
     Name = "server1a"
   }
@@ -10,10 +13,12 @@ resource "aws_instance" "server1a" {
 
 
 resource "aws_instance" "server1b" {
-  ami           = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
-  instance_type = "t2.micro"
-  key_name      = "vockey"
-  
+  ami            = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  instance_type  = "t2.micro"
+  key_name       = "vockey"
+  subnet_id      = module.vpc.public_subnets[1]
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
   tags = {
     Name = "server1b"
   }
@@ -23,7 +28,9 @@ resource "aws_instance" "server1c" {
   ami           = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
   instance_type = "t2.micro"
   key_name      = "vockey"
-  
+  subnet_id     = module.vpc.public_subnets[2]
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
   tags = {
     Name = "server1c"
   }
