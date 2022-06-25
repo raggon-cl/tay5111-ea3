a9f27f8400eb3de9066feefa69ef5272294de784
Rodrigo Aguilar <r.aguilarg@profesor.duoc.cl>
2022-06-25 14:31:47 +0000

    Add EC2 tags

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,10 @@ resource "aws_instance" "server1a" {
   ami           = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
   instance_type = "t2.micro"
   key_name      = "vockey"
+  
+  tags = {
+    Name = "server1a"
+  }
 }
 
 
@@ -9,12 +13,19 @@ resource "aws_instance" "server1b" {
   ami           = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
   instance_type = "t2.micro"
   key_name      = "vockey"
-
+  
+  tags = {
+    Name = "server1b"
+  }
 }
 
 resource "aws_instance" "server1c" {
   ami           = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
   instance_type = "t2.micro"
   key_name      = "vockey"
+  
+  tags = {
+    Name = "server1c"
+  }
 
 }
