d632bdea02bea894afdb01b28f393f909a75b747
Rodrigo Aguilar <r.aguilarg@profesor.duoc.cl>
2022-06-25 14:24:19 +0000

    Add ec2.tf and fmt correct

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,20 @@
 resource "aws_instance" "server1a" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  ami           = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
   instance_type = "t2.micro"
-  user_data
-  subnet_id
-  key_name = "vockey"
-  vpc_security_group_ids
- 
-  }
-  
-  
-  resource "aws_instance" "server1b" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
+  key_name      = "vockey"
+}
 
- 
-  }
-  
-  resource "aws_instance" "server1c" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
 
- 
-  }
+resource "aws_instance" "server1b" {
+  ami           = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  instance_type = "t2.micro"
+  key_name      = "vockey"
+
+}
+
+resource "aws_instance" "server1c" {
+  ami           = "ami-0cff7528ff583bf9a" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  instance_type = "t2.micro"
+  key_name      = "vockey"
+
+}
--- sg.tf
+++ sg.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "allow_ssh_http" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-ingress {
+  ingress {
     description      = "HTTP from Internet"
     from_port        = 80
     to_port          = 80
@@ -21,7 +21,7 @@ ingress {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-ingress {
+  ingress {
     description      = "HTTPS from Internet"
     from_port        = 443
     to_port          = 443
