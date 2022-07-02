58a4548d182efa35bba8f2143baa3a7be73abdc6
Rodrigo Aguilar <r.aguilarg@profesor.duoc.cl>
2022-07-02 18:38:35 +0000

    Modify key.tf

--- key.tf
+++ key.tf
@@ -8,10 +8,3 @@ resource "aws_key_pair" "server_key" {
   key_name   = "server_key"
   public_key = tls_private_key.server_key.public_key_openssh
 }
-
-# Saving Key Pair for ssh login for Client if needed
-#resource "null_resource" "save_server_key" {
-#  provisioner "local-exec" {
-#    command = "echo  ${tls_private_key.server_key.private_key_pem} > server_key.pem"
-#  }
-#}
