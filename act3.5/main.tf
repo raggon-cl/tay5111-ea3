84c1966d2471ff67968c9555a1af1d8d9a70ffb5
Rodrigo Aguilar <r.aguilarg@profesor.duoc.cl>
2024-06-18 23:34:09 +0000

    Update act3.5

--- act3.5/main.tf
+++ act3.5/main.tf
@@ -19,17 +19,14 @@ resource "aws_efs_file_system" "efs" {
 resource "aws_efs_mount_target" "efs_mount" {
   file_system_id = aws_efs_file_system.efs.id
   subnet_id      = "subnet-097f10e404e736c9f"
+  security_groups = ["${aws_security_group.efs-sg.id}"]
 }
 
-#resource "aws_subnet" "efs_subnets" {
-#  vpc_id     = "vpc-070a6e14d5d96c55d" # Reemplaza con el ID de tu VPC
-#  cidr_block = "172.31.32.0/20"
-#}
 
 resource "aws_security_group" "efs-sg" {
-  name        = "efs_security_group"
-#  description = "Permite tráfico NFS desde la instancia EC2"
-  vpc_id      = "vpc-070a6e14d5d96c55d"
+  name = "efs_security_group"
+  #  description = "Permite tráfico NFS desde la instancia EC2"
+  vpc_id = "vpc-070a6e14d5d96c55d"
 
   ingress {
     from_port   = 2049
@@ -37,7 +34,7 @@ resource "aws_security_group" "efs-sg" {
     protocol    = "tcp"
     cidr_blocks = ["172.31.32.0/20"]
   }
-  
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -61,6 +58,7 @@ resource "aws_instance" "ec2" {
 
   subnet_id              = "subnet-097f10e404e736c9f"
   vpc_security_group_ids = [aws_security_group.efs-sg.id]
+  depends_on      = [aws_efs_mount_target.efs_mount]
 
   # Script de usuario para montar EFS al iniciar
   user_data = <<-EOF
